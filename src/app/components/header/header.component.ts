fc0d9a141e19de0cc88c35681fd4e7e34abc70ca
Vova-code <vdoche@live.fr>
2024-12-16 14:58:02 +0100

    Chapter 1 : Built foundation with header and Material

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterModule} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    RouterModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
